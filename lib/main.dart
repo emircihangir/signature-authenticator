5877317e26bd7e0f1218f0807e7608b6344554a0
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-07-25 15:59:23 +0300

    Finish main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:sauth/sample.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+Future<Interpreter> loadModel() {
+  return Interpreter.fromAsset('assets/CONV007.tflite');
+}
 
 class PointsModel extends ChangeNotifier {
   List<Offset> points = [];
@@ -16,8 +23,18 @@ class PointsModel extends ChangeNotifier {
   }
 }
 
+class ModelOutputModel extends ChangeNotifier {
+  double _value = 0.5;
+  double get value => _value;
+  set value(double value) {
+    _value = value;
+    notifyListeners();
+  }
+}
+
 List trace = [];
 DateTime? startedDrawing;
+late Interpreter interpreter;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,9 +43,14 @@ Future<void> main() async {
   // Hide the system navigation bar
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
+  interpreter = await loadModel();
+
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (context) => PointsModel())],
+      providers: [
+        ChangeNotifierProvider(create: (context) => PointsModel()),
+        ChangeNotifierProvider(create: (context) => ModelOutputModel()),
+      ],
       builder: (context, child) => AndroidApp(),
     ),
   );
@@ -67,13 +89,55 @@ class HomePage extends StatelessWidget {
         Consumer<PointsModel>(
           builder: (context, value, child) {
             return IconButton(
-              onPressed: (value.points.isEmpty) ? null : infer,
+              onPressed: (value.points.isEmpty) ? null : () => infer(context),
               icon: Icon(Icons.done_rounded),
             );
           },
         ),
       ],
-      body: whiteBoard(context),
+      body: Stack(
+        children: [
+          whiteBoard(context),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(padding: const EdgeInsets.all(20.0), child: Text("0")),
+                  SizedBox(
+                    height: 2,
+                    width: 300,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Padding(padding: const EdgeInsets.all(20.0), child: Text("1")),
+                ],
+              ),
+            ),
+          ),
+          Consumer<ModelOutputModel>(
+            builder: (context, value, child) {
+              return Positioned(
+                left: (MediaQuery.of(context).size.width / 2) - 10 - 150 + (300 * value.value),
+                bottom: (60 - 20) / 2,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
@@ -135,4 +199,19 @@ void clearCanvas(BuildContext context) {
   trace.clear();
 }
 
-void infer() {}
+void infer(BuildContext context) {
+  Sample sample = Sample(trace);
+  // debugger();
+  final modelInput = [sample.processedInput];
+  final modelOutput = List.filled(1 * 1, 0.0).reshape([1, 1]);
+
+  // log('Input shape: ${interpreter.getInputTensor(0).shape}');
+  // log('Output shape: ${interpreter.getOutputTensor(0).shape}');
+
+  interpreter.run(modelInput, modelOutput);
+
+  log("Model output: ${modelOutput[0][0]}");
+  Provider.of<ModelOutputModel>(context, listen: false).value = modelOutput[0][0];
+
+  clearCanvas(context);
+}
